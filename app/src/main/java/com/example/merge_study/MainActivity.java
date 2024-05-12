6f0df2502848475531ce6240427774b290c951ce
gunoo_desktop <gunwoo405@naver.com>
2024-05-12 23:16:45 +0900

    TDL 병합

--- app/src/main/java/com/example/merge_study/MainActivity.java
+++ app/src/main/java/com/example/merge_study/MainActivity.java
@@ -18,29 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-//        XML코드에 onclick를 넣지 않아도 ID를 찾아서 클릭 기능
-//        Button calculator_btn = (Button) findViewById(R.id.calculator_btn);
-//        calculator_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent calculatorintent = new Intent(getApplicationContext(), CalculatorActivity.class);
-//
-//                startActivity(calculatorintent);
-//            }
-//        });
-
-//        XML코드에 onclick를 넣지 않아도 ID를 찾아서 클릭 기능
-//        Button tdl_btn = (Button) findViewById(R.id.TDL_btn);
-//        tdl_btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent TDLintent = new Intent(getApplicationContext(), TDLActivity.class);
-//                startActivity(TDLintent);
-//            }
-//        });
     }
-    public void btnclick(View v) {
+    public void btnClick(View v) {
         Button calculator_btn = (Button) findViewById(R.id.calculator_btn);
         Button tdl_btn = (Button) findViewById(R.id.TDL_btn);
 
